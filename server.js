e2555494ebe6ee20baca35b1bdc75314b966ac6e
akanellop <akanellop42@gmail.com>
2022-10-05 01:11:10 +0300

    WIP user pages implementation
    pages for login,signup,user info
    add views and model
    error message and header partials

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 mongoose.connect(process.env.DATABASE_URL);
@@ -12,13 +13,16 @@ db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 const indexRouter = require("./routes/index");
+const userRouter = require("./routes/users");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 
 app.use("/", indexRouter);
+app.use("/users", userRouter);
 
 app.listen(process.env.PORT || 3000);
